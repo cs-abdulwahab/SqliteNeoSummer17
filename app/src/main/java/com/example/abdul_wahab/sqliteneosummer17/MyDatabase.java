5590b7376f6e160c418ef9dea5219bb414988691
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-08-04 09:59:09 +0500

    Initial commit

---
+++ app/src/main/java/com/example/abdul_wahab/sqliteneosummer17/MyDatabase.java
@@ -0,0 +1,12 @@
+package com.example.abdul_wahab.sqliteneosummer17;
+
+import android.app.Application;
+
+/**
+ * Created by Abdul-Wahab on 8/4/2017.
+ */
+
+public class MyDatabase extends Application {
+
+
+}
